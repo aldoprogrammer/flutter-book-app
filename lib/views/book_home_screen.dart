adaee3f7f1e8cccb5bce0e2fc3e2951d6fb274c8
aldoprogrammer <aldobesma@gmail.com>
2024-09-17 21:03:34 +0700

    update

--- lib/views/book_home_screen.dart
+++ lib/views/book_home_screen.dart
@@ -33,39 +33,108 @@ class BooksHome extends StatelessWidget {
             Container(
               color: Colors.white,
               padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-              child: Column(children: [
-                SizedBox(
-                  height: 240,
-                  child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 240,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: booklist.length,
+                          itemBuilder: (context, index) {
+                            final book = booklist[index];
+                            return book.seller
+                                ? GestureDetector(
+                                    onTap: () {
+                                      // Handle tap event here
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(right: 15),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: Image.network(
+                                          book.showImage,
+                                          height: 250,
+                                          width: 350,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                : const SizedBox
+                                    .shrink(); // If not a seller, return an empty widget
+                          }),
+                    ),
+                    BestSeller(),
+                    const Text(
+                      "Best Sellers",
+                      style: TextStyle(fontSize: 16, color: Colors.black54),
+                    ),
+                    const Divider(
+                      color: Colors.black54,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    )
+                  ]),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "New Book List",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 325,
+                    child: ListView.builder(
                       itemCount: booklist.length,
+                      scrollDirection: Axis.horizontal,
                       itemBuilder: (context, index) {
                         final book = booklist[index];
-                        return book.seller
-                            ? GestureDetector(
-                                onTap: () {
-                                  // Handle tap event here
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.only(right: 15),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(10),
-                                    child: Image.network(
-                                      book.showImage,
-                                      height: 250,
-                                      width: 350,
-                                      fit: BoxFit.cover,
-                                    ),
+                        return GestureDetector(
+                          onTap: () {},
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 15),
+                            child: Column(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.network(
+                                    book.cover,
+                                    height: 220,
+                                    width: 150,
+                                    fit: BoxFit.cover,
                                   ),
                                 ),
-                              )
-                            : SizedBox
-                                .shrink(); // If not a seller, return an empty widget
-                      }),
-                )
-              ]),
+                                SizedBox(
+                                  height: 10,
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ));
+  }
+
+  SizedBox BestSeller() {
+    return const SizedBox(
+      height: 15,
+    );
   }
 }
